dc2ac7332c2a03a0b4e757b61b39ce34b4be2ef3
Crispussia <85911348+Crispussia@users.noreply.github.com>
2022-03-08 16:27:43 +0100

    Apprentissage_Base_Django

---
+++ articles/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    
+    path('articles/', views.articles_view, name='articles'),
+   
+]
